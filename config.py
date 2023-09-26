a313c92a7470c4aa76f16f7f6b5e3a07bdd6d3b0
Luffy <141612956+Luffy-2002@users.noreply.github.com>
2023-09-27 02:06:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 17193413
+api_id = 24781263
 
-api_hash = "5e50e6380252d9d487093bae4171bf91"
+api_hash = "41abdfd49a74666e24113d071db34c9d"
 
-bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+bot_token = "6428254883:AAHr7xEwmWregVDNVTq21Wg916pdyN2wshI"
 
 GROUP_TAG = "RC"
 
-sudo_users = [1606667548]
+sudo_users = [1358747363]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
